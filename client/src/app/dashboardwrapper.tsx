134ca0fa8a1640184bada477b240310e13633206
debian <tl@zasmlabs.com>
2024-10-06 10:15:43 +0530

    added dark light mode button

--- client/src/app/dashboardwrapper.tsx
+++ client/src/app/dashboardwrapper.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+import clsx from "clsx";
 
-const Dashboardwrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  },[isDarkMode]);
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64">
+      <main
+        className={clsx(
+          "flex w-full flex-col bg-gray-50 dark:bg-dark-bg",
+          isSidebarCollapsed ? "" : "md:pl-64",
+        )}
+      >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const Dashboardwrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 

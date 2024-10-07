ed47037e1dc54b4e062f42f9949b266901bcc0fa
debian <tl@zasmlabs.com>
2024-10-07 23:51:56 +0530

    tab buttons project page

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import clsx from "clsx";
-import { Grid3X3 } from "lucide-react";
+import { Clock, Filter, Grid3X3, List, Share2, Table2 } from "lucide-react";
 import React, { useState } from "react";
 
 type Props = {
@@ -23,6 +23,40 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
             setActiveTab={setActiveTab}
             activeTab={activeTab}
           />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="TimeLine"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table2 className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Filter className="h-5 w-5" />
+          </button>
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Share2 className="h-5 w-5" />
+          </button>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search Task"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            />
+            <Grid3X3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
+          </div>
         </div>
       </div>
     </div>
@@ -42,7 +76,7 @@ const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
   return (
     <div
       className={clsx(
-        "relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4",
+        "relative flex cursor-pointer items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4",
         isActive ? "after:bg-blue-600 dark:text-white" : "",
       )}
       onClick={() => setActiveTab(name)}
